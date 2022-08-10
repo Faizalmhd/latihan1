9986a643db6c61a5a56a0ee123193d6151439038
Faizal <109643012+Faizalmhd@users.noreply.github.com>
2022-08-10 08:17:21 +0700

    Update while.php

--- while.php
+++ while.php
@@ -5,6 +5,6 @@ $i = (int)readline("masukkan nilai awal :");
 $n = (int)readline("masukan  nilai ahir :");
 
 while ($i < $n) {
-    echo "perulangan while" . $i . "\n";
+    echo "ini perulangan while " . $i . "\n";
     $i++;
 }
